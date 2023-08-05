3e33ac9ff150db86a1167c5e41023679ca57567b
Koji Shiraishi <shiraco@gmail.com>
2023-08-05 23:01:44 +0900

    transfer method from callback

--- main.py
+++ main.py
@@ -47,65 +47,7 @@ app = FastAPI()
 app.mount("/woff", StaticFiles(directory="static/woff", html=True), name="woff")
 
 
-@app.post("/callback")
-async def callback(request: Request):
-    body_raw = await request.body()
-    body_json = await request.json()
-    headers = CaseInsensitiveDict(request.headers)
-    header_bot_id = headers.get("x-works-botid", "dummy")
-    signature = headers.get("x-works-signature", "dummy")
-
-    logger.debug("Headers: {}".format(headers))
-    logger.debug("Body: {}".format(body_json))
-
-    # Validation
-    signature = headers.get("x-works-signature")
-    if header_bot_id != bot_id or not lw.validate_request(body_raw, signature, bot_secret):
-        logger.warn("Invalid request")
-        return
-
-    # Receive message
-    if "channelId" in body_json["source"]:
-        from_channel_id = body_json["source"]["channelId"]
-        to_channel_id = from_channel_id
-        is_talk_room = True
-    else:
-        is_talk_room = False
-
-    from_user_id = body_json["source"]["userId"]
-    to_user_id = from_user_id
-
-    content = body_json["content"]
-    content_type = body_json["content"]["type"]
-
-    # Generate response
-    if content_type == "text":
-        content_text = content["text"]
-    elif content_type == "location":
-        content_text = content["address"]
-    elif content_type == "sticker":
-        content_text = "スタンプ送ります"
-    elif content_type == "image":
-        content_text = "画像送ります"
-    elif content_type == "file":
-        content_text = "ファイル送ります"
-    else:
-        content_text = "こんにちは"
-
-    # Generate response
-    res_text = chatgpt.generate_response(content_text)
-
-    # lineworks reply process
-    if "access_token" not in global_data:
-        # Get Access Token
-        logger.info("Get access token")
-        res = lw.get_access_token(client_id,
-                                  client_secret,
-                                  service_account_id,
-                                  privatekey,
-                                  SCOPE)
-        global_data["access_token"] = res["access_token"]
-
+def __send_message(is_talk_room: bool, res_text: str, to_channel_id: str, to_user_id: str):
     # send message
     for i in range(RETRY_COUNT_MAX):
         try:
@@ -151,9 +93,8 @@ async def callback(request: Request):
         else:
             break
 
-    # choose sticker
-    sticker_id, package_id = random.choice(lineworks_sticker.stickers)
 
+def __send_sticker(is_talk_room: bool, package_id: str, sticker_id: str, to_channel_id: str, to_user_id: str):
     # send sticker
     for i in range(RETRY_COUNT_MAX):
         try:
@@ -200,5 +141,72 @@ async def callback(request: Request):
             time.sleep(2 ** i)
         else:
             break
+
+
+@app.post("/callback")
+async def callback(request: Request):
+    body_raw = await request.body()
+    body_json = await request.json()
+    headers = CaseInsensitiveDict(request.headers)
+    header_bot_id = headers.get("x-works-botid", "dummy")
+    signature = headers.get("x-works-signature", "dummy")
+
+    logger.debug("Headers: {}".format(headers))
+    logger.debug("Body: {}".format(body_json))
+
+    # Validation
+    signature = headers.get("x-works-signature")
+    if header_bot_id != bot_id or not lw.validate_request(body_raw, signature, bot_secret):
+        logger.warn("Invalid request")
+        return
+
+    # Receive message
+    if "channelId" in body_json["source"]:
+        from_channel_id = body_json["source"]["channelId"]
+        to_channel_id = from_channel_id
+        is_talk_room = True
+    else:
+        is_talk_room = False
+
+    from_user_id = body_json["source"]["userId"]
+    to_user_id = from_user_id
+
+    content = body_json["content"]
+    content_type = body_json["content"]["type"]
+
+    # Generate response
+    if content_type == "text":
+        content_text = content["text"]
+    elif content_type == "location":
+        content_text = content["address"]
+    elif content_type == "sticker":
+        content_text = "スタンプ送ります"
+    elif content_type == "image":
+        content_text = "画像送ります"
+    elif content_type == "file":
+        content_text = "ファイル送ります"
+    else:
+        content_text = "こんにちは"
+
+    res_text = chatgpt.generate_response(content_text)
+
+    # lineworks reply process
+    if "access_token" not in global_data:
+        # Get Access Token
+        logger.info("Get access token")
+        res = lw.get_access_token(client_id,
+                                  client_secret,
+                                  service_account_id,
+                                  privatekey,
+                                  SCOPE)
+        global_data["access_token"] = res["access_token"]
+
+    __send_message(is_talk_room, res_text, to_channel_id, to_user_id)
+
+    # choose sticker
+    sticker_id, package_id = random.choice(lineworks_sticker.stickers)
+
+    __send_sticker(is_talk_room, package_id, sticker_id,
+                   to_channel_id, to_user_id)
 
     return {}
